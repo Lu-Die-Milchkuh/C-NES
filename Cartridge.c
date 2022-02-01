e5cb0e60d4354f841ebca8dcbc5c38c83bfef1df
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-02-01 14:50:06 +0100

    Added ROM Loading

--- Cartridge.c
+++ Cartridge.c
@@ -7,35 +7,26 @@
 #endif
 
 #include "Cartridge.h"
+#include "Mapper.h"
 
-byte* cartridge_memory = NULL;
+
+// Pointer to Store Programm Memory of ROM
+byte* cartridge_pgr_memory = NULL;
+
+// Pointer to Store Character Memory of ROM
+byte* cartridge_chr_memory = NULL;
+
 
 
 void loadROM(char* path) {
     FILE* file = fopen(path,"rb");
-    size_t file_size = 0;
 
     if(file == NULL) 
     {
         printf("Failed to load File!");
         exit(-1);
     }
-
-    fseek(file,0,SEEK_END);
-    file_size = ftell(file);
-    fseek(file,0,SEEK_SET);
-
-    // Loading Cartridge into "memory"
-    cartridge_memory = malloc((file_size*sizeof(byte)));
-    
-    // Check if malloc failed
-    if(cartridge_memory == NULL)
-    {
-        printf("Failed to allocat memory!");
-        exit(-1);
-    }
-
-    fread(cartridge_memory,sizeof(byte),file_size,file);
+    MAPPER_INIT(file);
 
     fclose(file);
 }
@@ -43,8 +34,14 @@ void loadROM(char* path) {
 
 // Freeing allocated memory
 void unloadROM() {
-    if(cartridge_memory != NULL) {
-        free(cartridge_memory);
+    
+    if(cartridge_pgr_memory != NULL)
+    {
+        free(cartridge_pgr_memory);
     }
     
+    if(cartridge_chr_memory != NULL) 
+    {
+        free(cartridge_chr_memory);
+    }
 }
