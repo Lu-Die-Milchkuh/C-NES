b466810b730e13dfa770498fceabe9e0a6612a74
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-02-13 18:26:49 +0100

    Added Comments

--- Bus.h
+++ Bus.h
@@ -2,8 +2,8 @@ typedef unsigned char byte;
 typedef unsigned short word;
 
 
-// Write Data to Address in memory
+// Write Data to Address in memory, CPU Bus
 void write(byte data,word address);
 
-// Read data from given address
+// Read data from given address, CPU Bus
 byte read(word address);
