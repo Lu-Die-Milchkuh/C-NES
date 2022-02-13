43653f0e4f0314ec26d812e1460bf0203edd07db
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-02-13 18:25:07 +0100

    Implemented Mapper 000

--- Mapper.c
+++ Mapper.c
@@ -11,77 +11,45 @@
 
 byte MAPPER_ID = 0;
 
-HEADER rom_header;
-
-// Number of Banks
 byte PRG_BANKS = 0;
 byte CHR_BANKS = 0;
 
+byte* chr_rom = NULL;
+byte* prg_rom = NULL;
+
+byte MIRROR = HORIZONTAL;
+
+
+
 
 void MAPPER_INIT(FILE* file) {
-    size_t pgr_mem_size = 0;
-    size_t chr_mem_size = 0;
-
-    // Read Header into struct
-    fread(&rom_header,sizeof(rom_header),1,file); 
-    //getHeader();
-
-    // Checking if "Trainer" exists
-    if((rom_header.mapper1 & 0x04) == 1) 
-    {   // Trainer is stored at 0x7000-0x71FF
-        //offset = 16 + 512 -> Header + Trainer
-        fseek(file,512+16,SEEK_SET);
-    }
-
-    // Getting Mapper ID 
-    MAPPER_ID = ((rom_header.mapper2 >> 4) << 4) | (rom_header.mapper1 >> 4); 
-
-    // Allocating Memory for Programm Code
-    if(MAPPER_ID == 0) 
-    {   
-        pgr_mem_size = (sizeof(byte)*rom_header.prg_rom_size*16384);
-
-        // Value 0 means board uses CHR RAM
-        if(rom_header.chr_rom_size == 0) 
-        {
-            chr_mem_size = (sizeof(byte)*8192);
-        }
-        else {
-            chr_mem_size = (sizeof(byte)*8192*rom_header.chr_rom_size);
-        }
-        
-
-        cartridge_pgr_memory = malloc(pgr_mem_size);
-        cartridge_chr_memory = malloc(chr_mem_size);
-    }
     
-    if(cartridge_pgr_memory == NULL) 
-    {   
-        printf("Failed to allocat cartridge_pgr_memory!");
-        exit(-1);
-    }
-
-    if(cartridge_chr_memory == NULL)
-    {
-        printf("Failed to allocat cartridge_chr_memory!");
-        exit(-1);
-    }
-
-    fread(cartridge_pgr_memory,pgr_mem_size,1,file);
-
-    fread(cartridge_chr_memory,chr_mem_size,1,file);
+    
     
 }
 
 
-/*
-byte MAPPER_000_READ(word address) {
-    byte data = 0;
-    word mapped_address = 0;
-    mapped_address = address & (PRG_BANKS > 1 ? 0x7FFF : 0x3FFF);
 
-    data = cartridge_memory[mapped_address];
+byte MAPPER_000_CPU_READ(word address) {
+    byte data = 0;
+    word mapped_addr = 0;
+    if(address >= 0x8000 && address <= 0xFFFF)
+    {
+        mapped_addr = address & (PRG_BANKS > 1 ? 0x7FFF : 0x3FFF);
+        data = prg_rom[mapped_addr];
+    }
+    
     return data;
 }
-*/
+
+
+byte MAPPER_000_PPU_READ(word address) {
+    byte data = 0;
+
+    if(address >= 0x0000 && address <= 0x1FFF)
+    {
+        data = chr_rom[address];
+    }
+    return data; 
+}
 
