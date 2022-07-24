6a783e7c33bdb5d6e5b627eada96418f24141820
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-07-24 13:40:15 +0200

    Initial commit of Logging Library

--- include/log.h
+++ include/log.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define LOG_WRITE( ...) log_write(__FILE__, __LINE__ ,__VA_ARGS__)
+#define LOG_WRITE(...) log_write(__FILE__, __LINE__ ,__VA_ARGS__)
 // Create a Log File in a OS specific Path
 void log_init(void);
 
